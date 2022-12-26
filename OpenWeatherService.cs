b12589e32b659068fb04cafdf23c3873becbdb99
Adam <Adam@DESKTOP-931URV2>
2022-12-26 19:23:19 +0100

    Add project files.

---
+++ OpenWeatherService.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Newtonsoft.Json;
+using System.Collections.Concurrent;
+
+
+using Assignment_A1_01.Models;
+
+namespace Assignment_A1_01.Services
+{
+    public class OpenWeatherService
+    {
+        HttpClient httpClient = new HttpClient();
+
+        //Cache declaration
+        ConcurrentDictionary<(double, double, string), Forecast> cachedGeoForecasts = new ConcurrentDictionary<(double, double, string), Forecast>();
+        ConcurrentDictionary<(string, string), Forecast> cachedCityForecasts = new ConcurrentDictionary<(string, string), Forecast>();
+
+        // Your API Key
+        readonly string apiKey = "51672c3748bc566b99e3945223f4f78a";
+
+        //Event declaration
+        public event EventHandler<string> WeatherForecastAvailable;
+        protected virtual void OnWeatherForecastAvailable(string message)
+        {
+            WeatherForecastAvailable?.Invoke(this, message);
+        }
+        public async Task<Forecast> GetForecastAsync(string City)
+        {
+
+            //part of cache code here to check if forecast in Cache
+            //generate an event that shows forecast was from cache
+
+            https://openweathermap.org/current
+            var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var uri = $"https://api.openweathermap.org/data/2.5/forecast?q={City}&units=metric&lang={language}&appid={apiKey}";
+
+            var Cities = City;
+            var date = DateTime.Now.ToString("yyyy, MM, dd: HH:mm");
+            DateTime timestamp = DateTime.Now;
+            var key = (Cities, date);
+
+            if (!cachedCityForecasts.TryGetValue(key, out var forecast) && (timestamp.AddMinutes(1) > DateTime.Now))
+            {
+                forecast = await ReadWebApiAsync(uri);
+                cachedCityForecasts[key] = forecast;
+                WeatherForecastAvailable?.Invoke(forecast, $"New weather forecast for {City} available");
+            }
+            else
+            WeatherForecastAvailable?.Invoke(forecast, $"Cached weather forecast for {City} available");
+            return forecast;
+        }
+        public async Task<Forecast> GetForecastAsync(double latitude, double longitude)
+        {
+
+            var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var uri = $"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&units=metric&lang={language}&appid={apiKey}";
+            var longit = longitude;
+            var latit = latitude;
+            var date = DateTime.Now.ToString("yyyy, MM, dd: HH:mm");
+            DateTime timestamp = DateTime.Now;
+            var key = (longit, latit, date);
+
+
+            if (!cachedGeoForecasts.TryGetValue(key, out var forecast) && (timestamp.AddMinutes(1) > DateTime.Now))
+                //if (!cachedGeoForecasts.TryGetValue(key, out var forecast))
+                {
+                    forecast = await ReadWebApiAsync(uri);
+                    cachedGeoForecasts[key] = forecast;
+                    WeatherForecastAvailable?.Invoke(forecast, $"New weather forecast for {latitude}, {longitude} available");
+                }
+            else
+            WeatherForecastAvailable?.Invoke(forecast, $"Cached weather forecast for {latitude} {longitude} available");
+                return forecast;
+
+
+        }
+        private async Task<Forecast> ReadWebApiAsync(string uri)
+        {
+            //Read the response from the WebApi
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            WeatherApiData wd = await response.Content.ReadFromJsonAsync<WeatherApiData>();
+
+            var forecast = new Forecast
+            {
+                City = wd.city.name,
+                Items = wd.list.Select(f => new ForecastItem
+                {
+                    Temperature = f.main.temp,
+                    Description = f.weather[0].description,
+                    WindSpeed = f.wind.speed,
+                    DateTime = UnixTimeStampToDateTime(f.dt)
+
+
+                }).ToList()
+            };
+
+            return forecast;
+        }
+        private DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Collections.Concurrent;
+
+
+using Assignment_A1_01.Models;
+using Assignment_A1_01.Services;
+
+namespace Assignment_A1_01
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            
+            OpenWeatherService service = new OpenWeatherService();
+            service.WeatherForecastAvailable += WeatherReportDataAvailable;
+
+            //Register the event
+            //Your Code
+
+            Task<Forecast>[] tasks = { null, null, null, null };
+            Exception exception = null;
+            try
+            {
+
+                double latitude = 60.079444;
+                double longitude = 17.739722;
+
+                //Create the two tasks and wait for comletion
+                tasks[0] = service.GetForecastAsync(latitude, longitude);
+                tasks[1] = service.GetForecastAsync("Phoenix");
+
+                Task.WaitAll(tasks[0], tasks[1]);
+
+                tasks[2] = service.GetForecastAsync(latitude, longitude);
+                tasks[3] = service.GetForecastAsync("Phoenix");
+
+                //Wait and confirm we get an event showing cahced data avaialable
+                Task.WaitAll(tasks[2], tasks[3]);
+
+                Forecast forecast = await new OpenWeatherService().GetForecastAsync(latitude, longitude);
+                Forecast forecastCity = await new OpenWeatherService().GetForecastAsync("Phoenix");
+                Console.WriteLine();
+
+                Console.WriteLine($"Weather forecast for {forecast.City}");
+                var groupedWeatherList = forecast.Items.GroupBy(x => x.DateTime.Date.DayOfWeek, x => x).Distinct().ToList();
+
+                foreach (var forcast in groupedWeatherList)
+                {
+                    Console.WriteLine(forcast.Key);
+                    forcast.ToList().ForEach(x => Console.WriteLine($"- • {x.DateTime.TimeOfDay} {x.Description} Temp: {x.Temperature} C°, Wind: {x.WindSpeed} m/s"));
+                }
+                Console.WriteLine();
+                Console.WriteLine("----------------------------------------------------------------------");
+
+                Console.WriteLine($"Weather forecast for {forecastCity.City}");
+                var groupedWeatherListCity = forecastCity.Items.GroupBy(x => x.DateTime.Date.DayOfWeek, x => x).Distinct().ToList();
+
+                foreach (var forcastCity in groupedWeatherListCity)
+                {
+                    Console.WriteLine(forcastCity.Key);
+                    forcastCity.ToList().ForEach(x => Console.WriteLine($"- • {x.DateTime.TimeOfDay} {x.Description} Temp: {x.Temperature} C°, Wind: {x.WindSpeed} m/s"));
+                }
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+                Console.WriteLine(ex.Message);
+            }
+
+            foreach (var task in tasks)
+            {
+                if (task != null && task.IsFaulted)
+                {
+                    Console.WriteLine("Error, the task didn't finish!");
+                }
+                if(task != null && task.IsCompleted)
+                {
+                    Console.WriteLine("Task completed!");
+                }
+            }
+            void WeatherReportDataAvailable(object sender, string message)
+            {
+                Console.WriteLine($"Event message from weather service: {message}");
+            }
+        }
+    }
+}
